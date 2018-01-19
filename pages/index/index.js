5eb248de52e0adadc1bd3d9688794c9cef457d60
Misterbigbooo <Zeno@ZenodeMacBook-Pro.local>
2018-01-19 15:37:35 +0800

    添加新闻内容

--- pages/index/index.js
+++ pages/index/index.js
@@ -33,59 +33,38 @@ Page({
        return false
      }
      this.setData({currentTab:type})
+     this.getNewsList(type, 0)
   },
-  
-
-
-  //事件处理函数
-  bindViewTap: function() {
+  getNewsList(type = 'toutiao',page = 0){
+    var that = this
     wx.request({
       url: 'https://v.juhe.cn/toutiao/index',
-      data:{
-        type:'yule',
-        key:'4ce75156f52c84f38414d8853bc30f6f'
+      data: {
+        type: type,
+        key: '4ce75156f52c84f38414d8853bc30f6f',
       },
       method: 'GET',
       header: {
         "Content-Type": "application/json,application/json"
       },
-      success:function(res){
-       console.log(res)
+      success: function (res) {
+        console.log(res.data.result.data)
+        that.setData({
+          articles: res.data.result.data
+        })
       },
     })
+  },
+  
 
-    // wx.navigateTo({
-    //   url: '../logs/logs'
-    // })
+  onReady() {
+    this.getNewsList()
   },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+
   },
+  
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
